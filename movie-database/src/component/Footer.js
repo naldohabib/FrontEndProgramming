64ae558d940aab74086ce4d465e02a6d5c83dcc1
Naldo habib Nawawi <naldohabib@gmail.com>
2022-03-19 19:42:11 +0700

    Update

---
+++ movie-database/src/component/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <h2>Copyright Naldo Habib Nawawi</h2>
+      <p>Website ini dibuat menggunakan ReactJS</p>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ movie-database/src/component/Main.js
@@ -0,0 +1,19 @@
+import Hello from './Hello'
+
+function Main() {
+  return (
+    <main>
+      {/**
+       * Mengirim props ke component Hello.
+       * nama props: name
+       */}
+      <Hello name="Aufa" />
+      <Hello name="Mikel" />
+      <Hello name="Hannah" />
+      <Hello name="Jonas" />
+      <Hello name="Martha" />
+    </main>
+  );
+}
+
+export default Main
